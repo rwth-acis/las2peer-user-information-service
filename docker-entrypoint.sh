491d4f0b4df1fa920a66276d205aa9ba280c639c
Philipp Dolif <git.phil.cd@gmail.com>
2021-10-24 13:15:01 +0200

    Try l2p 1.2-SNAPSHOT with Java 17

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,6 +11,7 @@ fi
 export SERVICE_VERSION=$(awk -F "=" '/service.version/ {print $2}' etc/ant_configuration/service.properties)
 export SERVICE_NAME=$(awk -F "=" '/service.name/ {print $2}' etc/ant_configuration/service.properties)
 export SERVICE_CLASS=$(awk -F "=" '/service.class/ {print $2}' etc/ant_configuration/service.properties)
+export CORE_VERSION=$(awk -F "=" '/core.version/ {print $2}' etc/ant_configuration/service.properties)
 export SERVICE=${SERVICE_NAME}.${SERVICE_CLASS}@${SERVICE_VERSION}
 
 # wait for any bootstrap host to be available
@@ -29,7 +30,7 @@ fi
 echo external_address = $(curl -s https://ipinfo.io/ip):${LAS2PEER_PORT} > etc/pastry.properties
 # prevent glob expansion in lib/*
 set -f
-LAUNCH_COMMAND='java -cp lib/* i5.las2peer.tools.L2pNodeLauncher -s service -p '"${LAS2PEER_PORT} ${SERVICE_EXTRA_ARGS}"
+LAUNCH_COMMAND='java -cp lib/* -jar lib/las2peer-bundle-'"${CORE_VERSION}"'.jar -s service -p '"${LAS2PEER_PORT} ${SERVICE_EXTRA_ARGS}"
 if [[ ! -z "${BOOTSTRAP}" ]]; then
     LAUNCH_COMMAND="${LAUNCH_COMMAND} -b ${BOOTSTRAP}"
 fi
